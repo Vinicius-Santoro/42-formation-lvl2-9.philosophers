8b9e8274aeaebaac9ef76b074e2145588ed576b6
vnazioze <vinicius.rio@ifsp.edu.com.br>
2022-08-05 20:38:15 -0300

    feat: :rocket: add memset function

---
+++ function_tests/memset.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memset.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnazioze <vnazioze@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/05 20:35:42 by vnazioze          #+#    #+#             */
+/*   Updated: 2022/08/05 20:37:41 by vnazioze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+/*
+Memset library
+*/
+#include <string.h>
+
+#define  BUF_SIZE  20
+ 
+int main(void)
+{
+	char 	buffer[BUF_SIZE + 1];
+	char 	*string;
+
+	memset(buffer, 0, sizeof(buffer));
+	string = (char *) memset(buffer,'A', 10);
+	printf("\nBuffer contents 1: %s\n", string);
+	memset(buffer + 10, 'B', 10);
+	printf("\nBuffer contents 2: %s\n", buffer);
+}
+ 
