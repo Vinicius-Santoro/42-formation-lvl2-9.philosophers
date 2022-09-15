b53909a9903a088b3a14b27fd07dbc8f0791ce16
ViniciusSantoro <vinicius.rio@ifsp.edu.br>
2022-09-15 17:04:54 -0300

    feat: :rocket: add free function

--- function_tests/free.c
+++ function_tests/free.c
@@ -1,58 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   malloc.c                                           :+:      :+:    :+:   */
+/*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vnazioze <vnazioze@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/14 23:16:47 by vnazioze          #+#    #+#             */
-/*   Updated: 2022/09/14 23:16:47 by vnazioze         ###   ########.fr       */
+/*   Created: 2022/09/15 16:58:03 by vnazioze          #+#    #+#             */
+/*   Updated: 2022/09/15 16:58:03 by vnazioze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 /*
-Malloc library
+Free library
 */
 #include <stdlib.h>
  
 int main()
 {
- 
-    // This pointer will hold the
+  
+    // This pointer ptr will hold the
     // base address of the block created
     int *ptr;
-    int n, i;
- 
+    int n;
+  
     // Get the number of elements for the array
-    printf("Enter number of elements: ");
-    scanf("%d",&n);
-    printf("Entered number of elements: %d\n", n);
- 
+    printf("Enter number of Elements: ");
+    scanf("%d", &n);
+  
     // Dynamically allocate memory using malloc()
     ptr = (int*)malloc(n * sizeof(int));
- 
+  
     // Check if the memory has been successfully
-    // allocated by malloc or not
-    if (ptr == NULL)
-    {
-        printf("Memory not allocated.\n");
+    // allocated by calloc() or not
+    if (ptr == NULL) {
+        printf("Memory not allocated \n");
         exit(0);
     }
-    else
-    {
-        // Memory has been successfully allocated
+    // Memory has been successfully allocated
         printf("Memory successfully allocated using malloc.\n");
  
         // Get the elements of the array
-        for (i = 0; i < n; ++i)
+        for (int i = 0; i < n; ++i)
             ptr[i] = i + 1;
 
         // Print the elements of the array
         printf("The elements of the array are: ");
-        for (i = 0; i < n; ++i) 
+        for (int i = 0; i < n; ++i) 
             printf("%d ", ptr[i]);
-    }
- 
+    
+    free(ptr);
+    printf("\nMalloc Memory Successfully freed.");
+  
     return 0;
 }
