88a9592046de8e9ec0ee906cb0b6e1f5e83d5daa
ViniciusSantoro <vinicius.rio@ifsp.edu.br>
2022-09-29 15:40:45 -0300

    feat: :rocket: add flag for threads

--- philosophers/src/main.c
+++ philosophers/src/main.c
@@ -10,12 +10,52 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
-int	main(void)
+int mails = 0;
+pthread_mutex_t mutex;
+
+void *routine()
 {
-	char *name = "Vinicius";
+    for (int i = 0; i < 10000000; i++)
+    {
+        pthread_mutex_lock(&mutex);
+        /*
+        Critical section
+        If mutex are not applied, it may cause a race condition where the values
+        of variables may be unpredictable and vary depending on the timings of
+        context switches of the processes or threads.
+        Comment the line 12 and 21 to see the race condition.
+        */
+        mails++;
+        pthread_mutex_unlock(&mutex);
+    }
+    return (NULL);
+}
 
-	write(2, name, sizeof(*name * sizeof(char)));
-	return (0);
-} 
+int main(void)
+{
+    pthread_t th[4];
+    int i;
+    pthread_mutex_init(&mutex, NULL);
+    for (i = 0; i < 4; i++)
+    {
+        if (pthread_create(th + i, NULL, &routine, NULL) != 0)
+        {
+            perror("Failed to create thread");
+            return 1;
+        }
+        printf("Thread %d has started\n", i);
+    }
+    for (i = 0; i < 4; i++)
+    {
+        if (pthread_join(th[i], NULL) != 0)
+            return 2;
+        printf("Thread %d has finished execution\n", i);
+    }
+    pthread_mutex_destroy(&mutex);
+    printf("Number of mails: %d\n", mails);
+    return 0;
+}
